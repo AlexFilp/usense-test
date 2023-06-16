958cb2f1136c1bd5122ffb3a1bfd52e90e680888
Oleksandr FIlippov <filya170895@gmail.com>
2023-06-16 11:33:15 +0200

    fixes and updates

--- src/components/passDifficultyScale/passDifficultyScale.jsx
+++ src/components/passDifficultyScale/passDifficultyScale.jsx
@@ -11,34 +11,38 @@ import { useEffect, useState } from 'react';
 export const PassDifficultyScale = ({ password }) => {
   const [passState, setPassState] = useState('gray');
 
-  const onlyLetters = '^[a-zA-Z]+$';
-  const onlyDigits = '^[0-9]+$';
-  const onlySymbols = `^[!@#$%^&*()_+-=;':"\\|,.<>/?]+$`;
-  const lettersSymbols = `^(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+-=;':"\\|,.<>/?]).+$`;
-  const lettersDigits = `^(?=.*[a-zA-Z])(?=.*d).+$`;
-  const digitsSymbols = `^(?=.*d)(?=.*[!@#$%^&*()_+-=[]{};':"\\|,.<>/?]).+$`;
-  const all = `^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$`;
+  const onlyLetters = /^[a-zA-Z]+$/;
+  const onlyDigits = /^[0-9]+$/;
+  const onlySymbols = /^[!@#$%^&*()_+-=;':"\\|,.<>/?]+$/;
+  const lettersSymbols =
+    /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+-=;':"\\|,.<>/?]).+$/;
+  const lettersDigits = /^(?=.*[a-zA-Z])(?=.*d).+$/;
+  const digitsSymbols = /^(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+  const all = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+  // console.log(digitsSymbols.test('2312132#@@#@@#@#!@!'), 'digitsSymbols');
 
   const onPasswordChange = password => {
+    console.log(all.test(password));
     if (password.length === 0) {
       setPassState('gray');
       return;
     } else if (password.length < 8) {
       setPassState('red');
+    } else if (all.test(password)) {
+      setPassState('strong');
     } else if (
-      password.match(onlyLetters) ||
-      password.match(onlyDigits) ||
-      password.match(onlySymbols)
-    ) {
-      setPassState('easy');
-    } else if (
-      password.match(digitsSymbols) ||
-      password.match(lettersDigits) ||
-      password.match(lettersSymbols)
+      digitsSymbols.test(password) ||
+      lettersDigits.test(password) ||
+      lettersSymbols.test(password)
     ) {
       setPassState('medium');
-    } else if (password.match(all)) {
-      setPassState('strong');
+    } else if (
+      onlyLetters.test(password) ||
+      onlyDigits.test(password) ||
+      onlySymbols.test(password)
+    ) {
+      setPassState('easy');
     }
   };
 
